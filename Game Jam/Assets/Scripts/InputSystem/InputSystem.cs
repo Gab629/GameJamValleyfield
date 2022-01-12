3ffcebea63e398561642dede81d3051bc69b91db
Gabriel Gauthier <gab.gauthier2002@gmail.com>
2022-01-11 19:54:11 -0500

    Ajout d'une scène de test
    -Ajout de murs et de sol
    -Ajout du nouveau input system
    -Ajout d'un tag Ground sur le sol
    -Ajout d'un action map déjà prêt
    -Ajout d'un cube comme personnage

---
+++ Game Jam/Assets/Scripts/InputSystem/InputSystem.cs
@@ -0,0 +1,234 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/InputSystem/InputSystem.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputSystem : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputSystem()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputSystem"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerMovements"",
+            ""id"": ""64ba3968-b31e-46c8-a9f4-29dce75221eb"",
+            ""actions"": [
+                {
+                    ""name"": ""Movements"",
+                    ""type"": ""Value"",
+                    ""id"": ""1c8828fb-d0b0-4fe4-a92f-eb54527e2249"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""034085a9-2f53-4fa5-89e2-d7ec4d13eaa4"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""f3fbb2db-a483-4d79-896b-01ea2e8fd741"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Tap""
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""46585693-ec3f-41bb-9e24-503f32101d7d"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movements"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""499bd2ec-d05f-49bb-a7de-934ccb37f470"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movements"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""a6fe2766-fc0a-45bd-9086-f68ae0c34f61"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movements"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""8801a2ff-4c7d-47ce-b03b-033e50d644d3"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movements"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""24ed1d09-2cc9-458d-9445-b7d664a719a2"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movements"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""ddc0ff45-fbe0-4d88-8548-2241dfe95446"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4297c3c3-7b7d-4fcb-aa5e-6ed07ad3ee49"",
+                    ""path"": ""<Keyboard>/e"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerMovements
+        m_PlayerMovements = asset.FindActionMap("PlayerMovements", throwIfNotFound: true);
+        m_PlayerMovements_Movements = m_PlayerMovements.FindAction("Movements", throwIfNotFound: true);
+        m_PlayerMovements_Jump = m_PlayerMovements.FindAction("Jump", throwIfNotFound: true);
+        m_PlayerMovements_Dash = m_PlayerMovements.FindAction("Dash", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerMovements
+    private readonly InputActionMap m_PlayerMovements;
+    private IPlayerMovementsActions m_PlayerMovementsActionsCallbackInterface;
+    private readonly InputAction m_PlayerMovements_Movements;
+    private readonly InputAction m_PlayerMovements_Jump;
+    private readonly InputAction m_PlayerMovements_Dash;
+    public struct PlayerMovementsActions
+    {
+        private @InputSystem m_Wrapper;
+        public PlayerMovementsActions(@InputSystem wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movements => m_Wrapper.m_PlayerMovements_Movements;
+        public InputAction @Jump => m_Wrapper.m_PlayerMovements_Jump;
+        public InputAction @Dash => m_Wrapper.m_PlayerMovements_Dash;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerMovements; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerMovementsActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerMovementsActions instance)
+        {
+            if (m_Wrapper.m_PlayerMovementsActionsCallbackInterface != null)
+            {
+                @Movements.started -= m_Wrapper.m_PlayerMovementsActionsCallbackInterface.OnMovements;
+                @Movements.performed -= m_Wrapper.m_PlayerMovementsActionsCallbackInterface.OnMovements;
+                @Movements.canceled -= m_Wrapper.m_PlayerMovementsActionsCallbackInterface.OnMovements;
+                @Jump.started -= m_Wrapper.m_PlayerMovementsActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerMovementsActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerMovementsActionsCallbackInterface.OnJump;
+                @Dash.started -= m_Wrapper.m_PlayerMovementsActionsCallbackInterface.OnDash;
+                @Dash.performed -= m_Wrapper.m_PlayerMovementsActionsCallbackInterface.OnDash;
+                @Dash.canceled -= m_Wrapper.m_PlayerMovementsActionsCallbackInterface.OnDash;
+            }
+            m_Wrapper.m_PlayerMovementsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movements.started += instance.OnMovements;
+                @Movements.performed += instance.OnMovements;
+                @Movements.canceled += instance.OnMovements;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Dash.started += instance.OnDash;
+                @Dash.performed += instance.OnDash;
+                @Dash.canceled += instance.OnDash;
+            }
+        }
+    }
+    public PlayerMovementsActions @PlayerMovements => new PlayerMovementsActions(this);
+    public interface IPlayerMovementsActions
+    {
+        void OnMovements(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnDash(InputAction.CallbackContext context);
+    }
+}

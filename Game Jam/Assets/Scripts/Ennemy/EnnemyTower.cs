2ab44858c8e9c0018150d32587ea4679489e1551
Caroline Forget <caroline.forget23@gmail.com>
2022-01-12 09:18:27 -0500

    Systeme de detection pour la Tour/Lama

--- Game Jam/Assets/Scripts/Ennemy/EnnemyTower.cs
+++ Game Jam/Assets/Scripts/Ennemy/EnnemyTower.cs
@@ -15,6 +15,12 @@ public class EnnemyTower : MonoBehaviour
 
     // TEST
     private bool isSeeingPlayer = false;
+    private bool playerLeft = false;
+
+    public float attackRange = 6f;
+
+    //
+    private int i = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -25,20 +31,40 @@ public class EnnemyTower : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+        float distance = Vector3.Distance(transform.position, target.position);
+
         if (isSeeingPlayer) {
              if (target.position.x > transform.position.x) {
                 //vers la droite
                 transform.rotation = Quaternion.Euler(0,0,0);
 
-                slider.direction = Slider.Direction.RightToLeft;
+                slider.direction = Slider.Direction.LeftToRight;
+                Debug.Log("vers la droite");
 
             } else {
                 //vers la gauche
                 transform.rotation = Quaternion.Euler(0,180,0);
 
-                slider.direction = Slider.Direction.LeftToRight;
+                slider.direction = Slider.Direction.RightToLeft;
+                Debug.Log("vers la gauche");
+            }
+
+
+            if (distance > attackRange) {
+                isSeeingPlayer = false;
+                Invoke("RotateEnemyLeft", 2f); 
+                Debug.Log("joueur perdu");
             }
         }
+
+
+    //
+/*         while (i > 1) {
+            RotateEnemyRight();
+            Debug.Log(i);
+            i = 0;
+            Debug.Log(i);
+        } */
     }
 
 
@@ -58,14 +84,53 @@ public class EnnemyTower : MonoBehaviour
         transform.rotation =  Quaternion.Euler(0, 180, 0);
         slider.direction = Slider.Direction.RightToLeft;
 
-        Invoke("RotateEnemyLeft", 2f);
+/*         while (isSeeingPlayer) {
+             if (target.position.x > transform.position.x) {
+                //vers la droite
+                transform.rotation = Quaternion.Euler(0,0,0);
+
+                slider.direction = Slider.Direction.LeftToRight;
+                Debug.Log("vers la droite");
+
+            } else {
+                //vers la gauche
+                transform.rotation = Quaternion.Euler(0,180,0);
+
+                slider.direction = Slider.Direction.RightToLeft;
+                Debug.Log("vers la gauche");
+            }
+        } */
+
+        if (!isSeeingPlayer) {
+            Invoke("RotateEnemyLeft", 2f); 
+        }
+
     }
 
     private void RotateEnemyLeft() {
         transform.rotation =  Quaternion.Euler(0, 0, 0);
         slider.direction = Slider.Direction.LeftToRight;
 
-        Invoke("RotateEnemyRight", 2f);
+/*         while (isSeeingPlayer) {
+             if (target.position.x > transform.position.x) {
+                //vers la droite
+                transform.rotation = Quaternion.Euler(0,0,0);
+
+                slider.direction = Slider.Direction.LeftToRight;
+                Debug.Log("vers la droite");
+
+            } else {
+                //vers la gauche
+                transform.rotation = Quaternion.Euler(0,180,0);
+
+                slider.direction = Slider.Direction.RightToLeft;
+                Debug.Log("vers la gauche");
+            }
+        } */
+
+        if (!isSeeingPlayer) {
+            Invoke("RotateEnemyRight", 2f);
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -84,9 +149,29 @@ public class EnnemyTower : MonoBehaviour
         }
     } */
 
-    private void OnTriggerExit(Collider other) {
+/*     private void OnTriggerExit(Collider other) {
         isSeeingPlayer = false;
+
         Debug.Log("joueur parti");
+
+
+
+        i++;
+    } */
+
+    private void PlayerState() {
+/*         int i = 0;
+
+        while (playerLeft) {
+            Invoke("RotateEnemyRight", 2f);
+            playerLeft = false;
+
+            i++;
+            Debug.Log("Index est : " + i);
+        } */
+
+        
+
     }
 
 }

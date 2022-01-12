8ee2fc225fb8a26bc061b824fc8dd2a4be6b07bf
Gabriel Gauthier <gab.gauthier2002@gmail.com>
2022-01-12 10:47:09 -0500

    Ajout de convoyeur (bug personnage)

--- Game Jam/Assets/Scripts/Mouvements.cs
+++ Game Jam/Assets/Scripts/Mouvements.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Mouvements : MonoBehaviour
 {
+
+    // ============================== **
+    // SOURCES DES TUTORIELS
+    // ==============================
+    // Video URL : https://www.youtube.com/watch?v=hC1QZ0h4oco (conveyor)
+    // ============================== **
+
+
     // Variables pour les components du personnage
     private CharacterController controller;
     public PlayerInput playerInput;
@@ -32,6 +40,8 @@ public class Mouvements : MonoBehaviour
     private float playerSpeed = 3f;
     public Vector2 input;
     public Vector3 move;
+    private int onConveyor = 0;
+    private float onConveyorSpeed = 2f;
 
 
     //Variables pour le nouveau input system
@@ -47,8 +57,9 @@ public class Mouvements : MonoBehaviour
     private float glidingSpeed = -100f;
     private float isGliding;
 
+
     //Variable pour les commandes inversés
-    [SerializeField] private bool invertedCommands = false;
+    private bool invertedCommands = false;
 
 
 
@@ -89,6 +100,12 @@ public class Mouvements : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
         controller = GetComponent<CharacterController>();
         rbCharacter = GetComponent<Rigidbody>();
+
+        
+        // calculate the correct vertical position:
+        float correctHeight = controller.center.y + controller.skinWidth;
+        // set the controller center vector:
+        controller.center = new Vector3(0, correctHeight, 0);
     }
 
 
@@ -96,11 +113,10 @@ public class Mouvements : MonoBehaviour
     //------- Update is called once per frame -------//
     void Update()
     {   
-        Gliding(); //COMP GLIDING
         Movements();
+        Gliding(); //COMP GLIDING
         Dash(); //COMP DASH
         WallSlide(); //COMP WALLSLIDE
-        
     }
 
 
@@ -110,11 +126,62 @@ public class Mouvements : MonoBehaviour
     {
         Jump();
         DoubleJump(); //COMP DOUBLE JUMP
+        OnConveyor();
 
     }
 
 
-    
+
+    void OnCollisionEnter(Collision collision){
+        if(collision.transform.tag == "convLeft")
+        {
+            onConveyor = -1;
+            playerSpeed = 0.1f;
+        }
+        else if(collision.transform.tag == "convRight")
+        {
+            onConveyor = 1;
+            playerSpeed = 0.1f;
+        }
+    }
+
+    void OnCollisionExit(Collision collision){
+        if(collision.transform.tag == "convLeft" || collision.transform.tag == "convRight")
+        {
+            onConveyor = 0;
+            playerSpeed = 3f;
+        }
+    }
+
+    private void OnConveyor(){
+        if(onConveyor == -1)
+        {
+            playerVelocity.x -= onConveyorSpeed * Time.deltaTime;
+
+            if(playerVelocity.x <= -onConveyorSpeed)
+            {
+                 playerVelocity.x = -onConveyorSpeed;
+            }
+
+        }
+        else if(onConveyor == 1)
+        {
+            playerVelocity.x += onConveyorSpeed * Time.deltaTime;
+
+            if(playerVelocity.x >= onConveyorSpeed)
+            {
+                 playerVelocity.x = onConveyorSpeed;
+            }
+
+        }
+        else
+        {
+            playerVelocity.x = 0f;
+        }
+    }
+
+
+
     //------- Cette fonction detecte si le bouton Espace est enfonce -------//
     private void JumpButton(InputAction.CallbackContext context)
     {
@@ -125,11 +192,17 @@ public class Mouvements : MonoBehaviour
         Invoke("JumpButtonCanceled", 0.05f);
         
     }
+
+
+
     //------- Cette fonction reduit le nombre de multipleJump -------//
     private void LessJumpMultiple(){
         
         multipleJump --;
     }
+
+
+
     //------- Cette fonction est call sert à remettre les bools pour doubleJump -------//
     private void JumpButtonCanceled()
     {
@@ -137,6 +210,8 @@ public class Mouvements : MonoBehaviour
         jumpCancelled = true;
         JumpBool = false;
     }
+
+
 
     //------- Cette fonction fait sauter le personnage -------//
     private void Jump()
@@ -161,11 +236,12 @@ public class Mouvements : MonoBehaviour
  
     }
 
+
+
     //------- Cette fonction permet de faire un double saut -------//
     private void DoubleJump(){
 
         //Si le joueur appuie sur espace, si il lui reste des sauts et qu'il a deja relacher espace une fois
-        
         if(JumpBool == true && multipleJump >= 0 && jumpCancelled && wallTouched <= 0){
             //Le joueur peut sauter a nouveau dans les airs
             playerVelocity.y += doubleJumpForce * Time.deltaTime;
@@ -181,7 +257,6 @@ public class Mouvements : MonoBehaviour
             wallTouched --;
         }
 
-
         //Si le compteur de saut est plus grand que 1
         if(multipleJumpCounter >= 1){
             //Le joueur ne peut pas sauter a nouveau
@@ -194,6 +269,8 @@ public class Mouvements : MonoBehaviour
             multipleJump = 0;
         }
     }
+
+
 
 //------- Cette fonction permet de faire une glissade sur le mur -------//
     private void WallSlide(){
@@ -213,6 +290,8 @@ public class Mouvements : MonoBehaviour
         
     }
 
+
+
     //------- Cette fonction detecte si le bouton droit de la souris est enfonce -------//
     private void GlidingCharacter(InputAction.CallbackContext context){
             isGliding = context.ReadValue<float>();
@@ -226,10 +305,7 @@ public class Mouvements : MonoBehaviour
     }
 
 
-    
-
-
-
+  
     //------- Cette fonction detecte si le bouton shift est enfonce -------//
     private void DashCharacter(InputAction.CallbackContext context){
         dashBool = true;

ab66d310d72b3e1442779298826038d3ba2215d3
Audrey Langlois <azpmaggie@gmail.com>
2022-01-13 01:51:09 -0500

    Ajout de sons et banque de sons

--- Game Jam/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Game Jam/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,11 +11,6 @@
 
 namespace AK
 {
-    namespace EVENTS
-    {
-        static const AkUniqueID FOOTSTEPS = 2385628198U;
-    } // namespace EVENTS
-
     namespace SWITCHES
     {
         namespace FOOTSTEPS

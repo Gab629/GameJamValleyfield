7812ae286008cb002084f8db56d6dd933678b9c9
Audrey Langlois <azpmaggie@gmail.com>
2022-01-13 02:39:26 -0500

    Sons Footstep Terminer

--- Game Jam/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Game Jam/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,11 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+    } // namespace EVENTS
+
     namespace SWITCHES
     {
         namespace FOOTSTEPS
